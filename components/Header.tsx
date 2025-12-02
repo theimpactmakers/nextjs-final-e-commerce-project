ee66788308a7893b6526e28eb2c92d2cb3ec5cce
MuhammedFatihCakir <muhammed.cakir@dci-student.org.com>
2025-12-02 09:16:26 +0100

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -341,6 +341,28 @@ export default function Header() {
               </div>
             </div>
 
+            {/* Angebote / Promotions Link */}
+            <div className="relative group inline-block">
+              <Link
+                href="/promotions"
+                className="relative inline-flex items-center h-9 px-2 transition-colors text-foreground hover:text-[hsl(33,100%,37%)]!"
+              >
+                <span className="flex items-center gap-1">
+                  <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                  </svg>
+                  Angebote
+                </span>
+                <span
+                  className="absolute left-1/2 -translate-x-1/2 bottom-0 h-[3px] w-11/12 origin-left scale-x-0 group-hover:scale-x-100 -translate-y-1 transform transition-transform duration-300 rounded"
+                  style={{
+                    backgroundImage:
+                      "linear-gradient(90deg, hsl(33 100% 37%) 0%, hsl(38 100% 50%) 50%, hsl(33 100% 37%) 100%)",
+                  }}
+                ></span>
+              </Link>
+            </div>
+
             {/* Specials mit Dropdown */}
             <div className="relative group inline-block">
               <Link
@@ -977,6 +999,16 @@ export default function Header() {
                   Senior
                 </Link>
               </div>
+              <Link
+                href="/promotions"
+                onClick={() => setMobileOpen(false)}
+                className="block text-lg font-medium hover:text-[hsl(var(--accent))] flex items-center gap-2"
+              >
+                <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                Angebote
+              </Link>
               <Link
                 href="/specials"
                 onClick={() => setMobileOpen(false)}
